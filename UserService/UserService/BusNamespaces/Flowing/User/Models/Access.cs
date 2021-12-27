df85a43e0f4fa422dc568c0ac3de09a2aaf5f1aa
Luca Nicolini <mithosk85@gmail.com>
2021-12-27 15:23:40 +0100

    Create Access.cs
    access model for login

---
+++ UserService/UserService/BusNamespaces/Flowing/User/Models/Access.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace UserService.BusNamespaces.Flowing.User.Models
+{
+    public class Access
+    {
+        public Guid UserId { get; set; }
+        public Guid AccessKey { get; set; }
+    }
+}
