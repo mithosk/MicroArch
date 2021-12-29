793bf04bdd8f6b248768baf460cf8678f65ac6a6
Luca Nicolini <mithosk85@gmail.com>
2021-12-29 14:38:18 +0100

    new login validation for refresh token

--- SharingGateway/SharingGateway/Validators/AuthorizationValidator.cs
+++ SharingGateway/SharingGateway/Validators/AuthorizationValidator.cs
@@ -7,10 +7,28 @@ namespace SharingGateway.Validators
     {
         public AuthorizationValidator()
         {
-            RuleFor(aut => aut.Email).NotEmpty().EmailAddress();
-            RuleFor(aut => aut.Password).NotEmpty();
+            //login authentication
+            RuleFor(aut => aut.Email).NotEmpty().EmailAddress()
+                .When(aut => !aut.RefreshToken.HasValue);
+
+            RuleFor(aut => aut.Password).NotEmpty()
+                .When(aut => !aut.RefreshToken.HasValue);
+
+            RuleFor(aut => aut.UserId).Null()
+               .When(aut => !aut.RefreshToken.HasValue);
+
+            //refresh authentication
+            RuleFor(aut => aut.Email).Null()
+                .When(aut => aut.RefreshToken.HasValue);
+
+            RuleFor(aut => aut.Password).Null()
+                .When(aut => aut.RefreshToken.HasValue);
+
+            RuleFor(aut => aut.UserId).NotEmpty()
+               .When(aut => aut.RefreshToken.HasValue);
+
+            //both
             RuleFor(aut => aut.Token).Null();
-            RuleFor(aut => aut.UserId).Null();
         }
     }
 }
