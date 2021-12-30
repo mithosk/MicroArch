56248fac5f21fbeff0a25fc96688025d19b67a04
Luca Nicolini <mithosk85@gmail.com>
2021-12-30 10:03:16 +0100

    utility for mock controller header

--- SharingGateway/SharingGateway.Test/TestBase.cs
+++ SharingGateway/SharingGateway.Test/TestBase.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,6 +15,17 @@ namespace SharingGateway.Test
             return JsonConvert.DeserializeObject<List<TItem>>(File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\Json\" + fileName))
                 .Select(ite => new object[] { ite })
                 .ToList();
+        }
+
+        protected static void AddRequestHeader(Controller controller, string key, string value)
+        {
+            HttpContext httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers.Add(key, value);
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
         }
     }
 }
