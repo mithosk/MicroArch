011e5d328ad1fd92e1605cabbcb4a53c81b78f4d
Luca Nicolini <mithosk85@gmail.com>
2021-12-30 10:11:24 +0100

    reset refresh-token test

--- SharingGateway/SharingGateway.Test/Controllers/AuthorizationControllerTest.cs
+++ SharingGateway/SharingGateway.Test/Controllers/AuthorizationControllerTest.cs
@@ -1,10 +1,12 @@
 ﻿using AgileServiceBus.Interfaces;
 using FakeItEasy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using SharingGateway.Controllers;
 using SharingGateway.Models;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Threading.Tasks;
 using Xunit;
 using FlowingUserModels = SharingGateway.BusNamespaces.Flowing.User.Models;
@@ -71,6 +73,40 @@ namespace SharingGateway.Test.Controllers
             Assert.NotNull(response.Value.Token);
             Assert.Equal(refreshToken, response.Value.RefreshToken);
             Assert.Equal(userId, response.Value.UserId);
+        }
+
+        [Fact]
+        public async Task RefreshTokenReset()
+        {
+            Guid userId = Guid.NewGuid();
+
+            //bus fake
+            IGatewayBus bus = A.Fake<IGatewayBus>();
+            A.CallTo(() => bus.RequestAsync<bool>(A<FlowingUserRequests.ResetAccessKey>.Ignored, A<ITraceScope>.Ignored))
+                .Returns(true);
+
+            //trace scope fake
+            ITraceScope traceScope = A.Fake<ITraceScope>();
+
+            //jwt token fake
+            JwtSecurityTokenHandler tokenHandler = new();
+            SecurityToken securityToken = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Claims = new Dictionary<string, object>
+                {
+                    { "UserId", userId.ToString() }
+                }
+            });
+
+            string token = tokenHandler.WriteToken(securityToken);
+
+            //execution
+            AuthorizationController controller = new(bus, traceScope);
+            AddRequestHeader(controller, "Authorization", $"Bearer {token}");
+            ActionResult response = await controller.Delete(userId);
+
+            //check
+            Assert.IsType<OkResult>(response);
         }
 
         public static List<object[]> LoadBodies()
