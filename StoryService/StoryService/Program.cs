496478b6a585cf1f4bb729cb62a4e7d1aaab046c
Luca Nicolini <mithosk85@gmail.com>
2022-01-03 11:56:41 +0100

    agile host-factory

--- StoryService/StoryService/Program.cs
+++ StoryService/StoryService/Program.cs
@@ -1,4 +1,13 @@
-﻿using Topshelf;
+﻿using AgileServiceBus.Additionals;
+using AgileServiceBus.Drivers;
+using AgileServiceBus.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using StoryService.BusNamespaces.Flowing.Story.Requests;
+using StoryService.Data;
+using StoryService.Data.Interfaces;
+using StoryService.Subscribers.Responders;
+using StoryService.Utilities.Logic;
+using StoryService.Validators.Requests;
 
 namespace StoryService
 {
@@ -6,16 +15,36 @@ namespace StoryService
     {
         public static void Main()
         {
-            HostFactory.Run(hco =>
+            HostFactory.Run(() =>
             {
-                hco.Service<Service>(sce =>
-                {
-                    sce.ConstructUsing(hso => new Service());
-                    sce.WhenStarted(ser => ser.Start());
-                    sce.WhenStopped(ser => ser.Stop());
-                });
+                IMicroserviceLifetime ml = new RabbitMQDriver(Env.Get("RABBITMQ_CONN_STR"));
 
-                hco.RunAsLocalService();
+
+
+
+
+                //responders
+                ml.Subscribe<PublishStoryResponder, PublishStory>(new PublishStoryValidator());
+                ml.Subscribe<SearchPOIResponder, SearchPOI>(new SearchPOIValidator());
+                ml.Subscribe<StoryDetailResponder, StoryDetail>(new StoryDetailValidator());
+                ml.Subscribe<StoryListResponder, StoryList>(new StoryListValidator());
+
+
+
+
+
+                //dependency injection
+                ml.Injection.AddDbContext<DataContext>();
+                ml.Injection.AddScoped<IDataContext>(spe => spe.GetService<DataContext>());
+                ml.Injection.AddScoped<INoSaveDataContext>(spe => spe.GetService<DataContext>());
+
+                ml.Injection.AddScoped<GeolocationUtility>();
+
+
+
+
+
+                return ml;
             });
         }
     }
