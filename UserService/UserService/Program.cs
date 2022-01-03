1b34859e754286f9d6ce1f109dc3d54ab37128d7
Luca Nicolini <mithosk85@gmail.com>
2022-01-03 11:08:26 +0100

    agile host-factory

--- UserService/UserService/Program.cs
+++ UserService/UserService/Program.cs
@@ -1,4 +1,16 @@
-﻿using Topshelf;
+﻿using AgileServiceBus.Additionals;
+using AgileServiceBus.Drivers;
+using AgileServiceBus.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using UserService.BusNamespaces.Flowing.Story.Events;
+using UserService.BusNamespaces.Flowing.User.Requests;
+using UserService.Data;
+using UserService.Data.Interfaces;
+using UserService.Subscribers.EventHandlers;
+using UserService.Subscribers.Responders;
+using UserService.Utilities.Logic;
+using UserService.Validators.Events;
+using UserService.Validators.Requests;
 
 namespace UserService
 {
@@ -6,16 +18,43 @@ namespace UserService
     {
         public static void Main()
         {
-            HostFactory.Run(hco =>
+            HostFactory.Run(() =>
             {
-                hco.Service<Service>(sce =>
-                {
-                    sce.ConstructUsing(hso => new Service());
-                    sce.WhenStarted(ser => ser.Start());
-                    sce.WhenStopped(ser => ser.Stop());
-                });
+                IMicroserviceLifetime ml = new RabbitMQDriver(Env.Get("RABBITMQ_CONN_STR"));
 
-                hco.RunAsLocalService();
+
+
+
+
+                //event handlers
+                ml.Subscribe<PublishedStoryEventHandler, PublishedStory>(null, new PublishedStoryValidator(), null, null);
+
+
+
+
+
+                //responders
+                ml.Subscribe<LoginResponder, Login>(new LoginValidator());
+                ml.Subscribe<ResetAccessKeyResponder, ResetAccessKey>(new ResetAccessKeyValidator());
+                ml.Subscribe<UserDetailResponder, UserDetail>(new UserDetailValidator());
+                ml.Subscribe<ValidateAccessKeyResponder, ValidateAccessKey>(new ValidateAccessKeyValidator());
+
+
+
+
+
+                //dependency injection
+                ml.Injection.AddDbContext<DataContext>();
+                ml.Injection.AddScoped<IDataContext>(spe => spe.GetService<DataContext>());
+                ml.Injection.AddScoped<INoSaveDataContext>(spe => spe.GetService<DataContext>());
+
+                ml.Injection.AddScoped<PasswordUtility>();
+
+
+
+
+
+                return ml;
             });
         }
     }
