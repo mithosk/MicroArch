73ea2a58c653ae62796cb7a6bcdc72dcd9a8c4fc
Luca Nicolini <mithosk85@gmail.com>
2022-01-04 17:22:27 +0100

    data migration for users

--- UserService/UserService/Migrations/20220104161822_CreateDatabase.cs
+++ UserService/UserService/Migrations/20220104161822_CreateDatabase.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 #nullable disable
 
@@ -71,6 +71,12 @@ namespace UserService.Migrations
                 table: "Users",
                 column: "ExternalId",
                 unique: true);
+
+            migrationBuilder.Sql(@"
+                INSERT INTO Users (ExternalId, Email, PasswordHash, [Name], Surname, RegistrationDate) VALUES ('7bb2891d-9645-4b2c-b02f-99fef892a699', 'mithosk85@gmail.com',              '918B9FD83DE4910112EAEC1E6AFB4FE5DDEBF5F26872B814895F1E8032FC5EBB6D42FA9708852C4A0D58874B32F314EE91BDB7624F83632A848916D84B384EF4', 'Luca', 'Nicolini', GETDATE())
+                INSERT INTO Users (ExternalId, Email, PasswordHash, [Name], Surname, RegistrationDate) VALUES ('b2524fde-43c4-4d6d-ad3f-559adf30c1d9', 'alessandro.minoccheri@flowing.it', '2D70DD5EB64AEA70E9F44B10B357A322F96CC85FBD20DCD3442D0306BA2D7433AFC1DDD36350857CC5EA3D8F917428C2735DBB10FE2F84711191A60CD891E4EF', 'Alessandro', 'Minoccheri', GETDATE())
+                INSERT INTO Users (ExternalId, Email, PasswordHash, [Name], Surname, RegistrationDate) VALUES ('446ae8cf-0610-4358-947f-35749f953b7e', 'michele.orselli@flowing.it',       '896F936BA8EE53DD669685D02F79EC50CDE05FBF96C1CAC5280088E670D286B373C22A6091E05DD3468DC88D3542868413903BD65DBB59CDAED6EB757FF63E5F', 'Michele', 'Orselli', GETDATE())
+            ");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
