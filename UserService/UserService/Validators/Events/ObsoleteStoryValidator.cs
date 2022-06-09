67a3c7f3242bdde9185a03a727254024d33cd832
Luca Nicolini <mithosk85@gmail.com>
2022-06-09 17:10:42 +0200

    Create ObsoleteStoryValidator.cs

---
+++ UserService/UserService/Validators/Events/ObsoleteStoryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using UserService.BusNamespaces.Flowing.Story.Events;
+
+namespace UserService.Validators.Events
+{
+    public class ObsoleteStoryValidator : AbstractValidator<ObsoleteStory>
+    {
+        public ObsoleteStoryValidator()
+        {
+            RuleFor(osb => osb.StoryId).NotEmpty();
+        }
+    }
+}
