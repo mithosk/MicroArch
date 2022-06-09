7290ca185acaf7019b1b84dd317cfc9a664257dc
Luca Nicolini <mithosk85@gmail.com>
2022-06-09 12:31:57 +0200

    Create ObsoleteStoriesValidator.cs

---
+++ StoryService/StoryService/Validators/Events/ObsoleteStoriesValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using StoryService.BusNamespaces.Flowing.Story.Events;
+using System;
+
+namespace StoryService.Validators.Events
+{
+    public class ObsoleteStoriesValidator : AbstractValidator<ObsoleteStories>
+    {
+        public ObsoleteStoriesValidator()
+        {
+            RuleFor(osb => osb.MinDate).NotEmpty().LessThanOrEqualTo(DateTime.UtcNow);
+        }
+    }
+}
