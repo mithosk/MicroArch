4a4a489b25f282f2ce1af7f6a7825d62f7cd8006
Luca Nicolini <mithosk85@gmail.com>
2022-06-09 17:26:36 +0200

    Create ObsoleteStoryEventHandlerTest.cs

---
+++ UserService/UserService.Test/Subscribers/EventHandlers/ObsoleteStoryEventHandlerTest.cs
@@ -0,0 +1,57 @@
+﻿using AgileServiceBus.Interfaces;
+using FakeItEasy;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Data;
+using UserService.Data.Interfaces;
+using UserService.Data.Models;
+using UserService.Subscribers.EventHandlers;
+using Xunit;
+using FlowingStoryEvents = UserService.BusNamespaces.Flowing.Story.Events;
+
+namespace UserService.Test.Subscribers.EventHandlers
+{
+    public class ObsoleteStoryEventHandlerTest : TestBase
+    {
+        [Theory]
+        [MemberData(nameof(LoadMessages))]
+        public async Task StoryRemoval(FlowingStoryEvents.ObsoleteStory message)
+        {
+            //data context fake
+            using (IDataContext dataContext = new DataContext(_dataOptions))
+            {
+                await dataContext.Stories.AddAsync(new Story
+                {
+                    ExternalId = message.StoryId
+                });
+
+                await dataContext.SaveChangesAsync();
+            }
+
+            //execution
+            using (IDataContext dataContext = new DataContext(_dataOptions))
+            {
+                ObsoleteStoryEventHandler hanlder = new(dataContext);
+                hanlder.TraceScope = A.Fake<ITraceScope>();
+                await hanlder.HandleAsync(message);
+            }
+
+            //check
+            using (IDataContext dataContext = new DataContext(_dataOptions))
+            {
+                Story story = await dataContext.Stories
+                    .Where(sto => sto.ExternalId == message.StoryId)
+                    .SingleOrDefaultAsync();
+
+                Assert.Null(story);
+            }
+        }
+
+        public static List<object[]> LoadMessages()
+        {
+            return LoadJson<FlowingStoryEvents.ObsoleteStory>("Events/ObsoleteStory.json");
+        }
+    }
+}
