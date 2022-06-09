9e191fac9c1818fd5dc083c0622624ffafacc791
Luca Nicolini <mithosk85@gmail.com>
2022-06-09 17:08:02 +0200

    Create ObsoleteStory.cs

---
+++ UserService/UserService/BusNamespaces/Flowing/Story/Events/ObsoleteStory.cs
@@ -0,0 +1,11 @@
+﻿using AgileServiceBus.Attributes;
+using System;
+
+namespace UserService.BusNamespaces.Flowing.Story.Events
+{
+    [BusNamespace(Directory = "Flowing", Subdirectory = "Story")]
+    public class ObsoleteStory
+    {
+        public Guid StoryId { get; set; }
+    }
+}
