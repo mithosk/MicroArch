b8baf2ba46d8cdedc25d8cce6c86e7e8f1f271c1
Luca Nicolini <mithosk85@gmail.com>
2022-06-09 17:13:44 +0200

    Create ObsoleteStoryEventHandler.cs

---
+++ UserService/UserService/Subscribers/EventHandlers/ObsoleteStoryEventHandler.cs
@@ -0,0 +1,38 @@
+﻿using AgileServiceBus.Interfaces;
+using System.Threading.Tasks;
+using UserService.BusNamespaces.Flowing.Story.Events;
+using UserService.Data.Interfaces;
+using UserService.Data.Models;
+using UserService.Data.Repositories;
+
+namespace UserService.Subscribers.EventHandlers
+{
+    public class ObsoleteStoryEventHandler : IEventHandler<ObsoleteStory>
+    {
+        private readonly IDataContext _dataContext;
+
+        public IMicroserviceBus Bus { get; set; }
+        public ITraceScope TraceScope { get; set; }
+
+        public ObsoleteStoryEventHandler(IDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task HandleAsync(ObsoleteStory message)
+        {
+            //find story
+            Story story;
+            using (TraceScope.CreateSubScope("FindStory"))
+                story = await _dataContext.Stories.FindByAsync(message.StoryId);
+
+            //delete story
+            if (story != null)
+                using (TraceScope.CreateSubScope("SaveChanges"))
+                {
+                    _dataContext.Stories.Remove(story);
+                    await _dataContext.SaveChangesAsync();
+                }
+        }
+    }
+}
