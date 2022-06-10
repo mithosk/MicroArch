cffd84d19b0a36acd4453d8641b65dc7976e8d84
Luca Nicolini <mithosk85@gmail.com>
2022-06-10 10:16:53 +0200

    Create ObsoleteStories.cs

---
+++ CleanerScheduler/CleanerScheduler/BusNamespaces/Flowing/Story/Events/ObsoleteStories.cs
@@ -0,0 +1,10 @@
+﻿using AgileServiceBus.Attributes;
+
+namespace CleanerScheduler.BusNamespaces.Flowing.Story.Events
+{
+    [BusNamespace(Directory = "Flowing", Subdirectory = "Story")]
+    public class ObsoleteStories
+    {
+        public DateTime MinDate { get; set; }
+    }
+}
